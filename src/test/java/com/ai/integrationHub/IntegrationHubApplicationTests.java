96ea7f51334807247647a7946e879733bf7e1b2d
harshrajsisodiya9-creator <harshrajsisodiya9@gmail.com>
2026-07-23 22:21:55 +0530

    Initial Commit

---
+++ src/test/java/com/ai/integrationHub/IntegrationHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.integrationHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
